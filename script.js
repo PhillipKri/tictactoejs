bda7a9749a1ae0f70885c5625e2255a16dd583a3
PhillipKri <irrationalman26@gmail.com>
2024-12-27 19:17:01 +0100

    Done, functionaility and design is finished. Though should refactor the code to abide SOLID principles

--- script.js
+++ script.js
@@ -1,42 +1,98 @@
 const Gameboard = (function () {
-    let board = document.querySelectorAll(".tile");
+    const get_board = () => {
+        return document.querySelectorAll(".tile");
+    }
 
+    const get_msg = () => {
+        return document.querySelector(".message")
+    }
     const reset = () => {
-        board.forEach((tile) => {
+        get_board().forEach((tile) => {
             tile.textContent = ""
-        });
+            tile.classList.remove("X")
+            tile.classList.remove("O")
+            get_msg().textContent = ""
+        })
     };
-    return {reset}
+    return {reset, get_board, get_msg}
 })();
 
-function mark (){
-    let board = document.querySelectorAll(".tile");
-    board.forEach((tile) => {
-        tile.addEventListener("click", () => {
-            tile.textContent = "X"
-        });
-    })
-    //console.log(board);
-}
+const createPlayer = (() => {
+    let whichPlayerTurn = false;
+    return {whichPlayerTurn}
+})();
+
+let gameOver = false
+
+const Game = (() => {
+
+    const gameMsg = document.querySelector(".message")
+    const mark =  () => {
+        Gameboard.get_board().forEach((tile) => {
+            tile.addEventListener("click", () => {
+                if(tile.textContent === "" && !gameOver){
+                let currentPlayer = createPlayer.whichPlayerTurn ? "X" : "O";
+                tile.textContent = currentPlayer;
+                tile.classList.add(currentPlayer);
+                if(check_win(currentPlayer)){
+                }
+                else if(isDraw()){
+                    Gameboard.get_msg().textContent= "It's a draw..."
+                    return;
+                }else {
+                    console.log("keep playing asshole")
+                    swap_players(currentPlayer)
+                }}
+            });
+        })}
+
+
+    const swap_players = () => {
+        createPlayer.whichPlayerTurn = !createPlayer.whichPlayerTurn;
+        Gameboard.get_msg().textContent = `It's is player ${createPlayer.whichPlayerTurn ? "X" : "O"}'s turn`
+    }
+
+    const check_win = (current) => {
+        const winning_combinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for(let i = 0; i < winning_combinations.length; i++){
+            const [a, b, c] = winning_combinations[i]
+            if(Gameboard.get_board()[a].textContent === current && Gameboard.get_board()[b].textContent === current && Gameboard.get_board()[c].textContent === current){
+                gameOver = true
+                Gameboard.get_msg().textContent = `"${current}" has won the game!`
+                return true
+            }
+        }
+        return false
+    }
+    const isDraw = () => {
+        return [...Gameboard.get_board()].every(tile => tile.classList.contains("X") || tile.classList.contains("O"))
+    }
+
+
+    return {mark, gameOver};
+})();
+
 const start_btn = document.querySelector(".start-btn");
 start_btn.addEventListener("click", () => {
-    //Game.start();
+    Game.mark();
 });
 
-function reset() {
-    Gameboard.reset();
-}
-
-// for(let i = 0; board.length; i++){
-//     board[i].addEventListener("click", () => {
-//         board[i].textContent = "X"
-//     });
-// }
 
 const restart_btn = document.querySelector(".restart-btn");
 restart_btn.addEventListener("click", () => {
-    reset()
+    Gameboard.reset();
+    gameOver = false
 });
 
-mark();
+
 
